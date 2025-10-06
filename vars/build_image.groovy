b45929fe69083e43a37fd709dd2352bb3578de8b
Ganesh Deshmukh <ganeshdeshmukh2505@gmail.com>
2025-10-06 12:15:26 +0530

    Update build_image.groovy

--- vars/build_image.groovy
+++ vars/build_image.groovy
@@ -1,5 +1,3 @@
-def call(string projectname , string tag , string username){
-  echo 'Building Image From Dockerfile'
-  sh "docker build -t notes-app:latest ."
-  sh "docker tag notes-app:latest ganeshd2505/notes-app:latest"
+def call(string ProjectName , string ImageTag , string DockerHubUser){
+ sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
